0cc0e4e9772be91459200f58bfa28bc329c1cab1
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-07-02 10:39:33 -0400

    added beginning and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -69,6 +69,13 @@ var fight = function(enemyName) {
   }
 };
 
+//function to start new game
+var startGame = function() {
+    //reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
 // fight each enemy-robot by looping over them and fighting them one at a time
 for (var i = 0; i < enemyNames.length; i++) {
   // if player is still alive, keep fighting
@@ -94,3 +101,29 @@ for (var i = 0; i < enemyNames.length; i++) {
     break;
   }
 }
+    //play again
+    startGame();
+};
+
+    //function to end the entire game
+    var endGame = function() {
+        //if player health is still alive, player wins!
+        if (playerHealth > 0) {
+            window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+        }
+        else {
+            window.alert("You've lost your robot in battle.");
+        }
+
+        //ask player if they'd like to play again
+        var playAgainConfirm = window.confirm("Would you like to play again?");
+
+        if (playAgainConfirm) {
+            //restart game
+            startGame();
+        }
+        else {
+            window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+        }
+    }
+startGame();
